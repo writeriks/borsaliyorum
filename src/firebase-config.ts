2c816e1e39db690d014c01944c3f7b3ca6fb6a84
sakirdemirhan <sakirdemirhan@outlook.com>
2024-05-28 22:30:41 +0200

    Add google authentication

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,7 +27,9 @@ const storage = getStorage(app);
 
 const db = getFirestore(app);
 
+const auth = getAuth(app);
+
 // TODO: Adding analytics gives error. investigate how to use analytics
 // const analytics = getAnalytics(app);
 
-export { app, db, storage };
+export { app, db, storage, auth };
