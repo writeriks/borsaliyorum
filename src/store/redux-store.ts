e51d1f5112832a295ded142081d354fbb345d467
writeriks <emirhaktan@gmail.com>
2024-05-31 22:17:48 +0200

    Initialize redux with persistor

---
+++ src/store/redux-store.ts
@@ -0,0 +1,44 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+import uiSlice from "./reducers/ui-reducer/ui-slice";
+
+const rootReducer = combineReducers({
+  ui: uiSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV != "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
+export default store;
+
+/* export type RootState = ReturnType<typeof store.getState>;
+
+export default store; */
