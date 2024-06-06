cbb80b39380c613b7678c5138d58d29ec7262c8c
sakirdemirhan <sakirdemirhan@outlook.com>
2024-06-06 20:10:13 +0200

    address comments

--- src/components/auth/reset-password.tsx
+++ src/components/auth/reset-password.tsx
@@ -56,11 +56,7 @@ export function ResetPassword({
         />
 
         <Button className="w-full" type="submit">
-          {isLoading ? (
-            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            ""
-          )}
+          {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
           Şifre yenileme isteği gönder
         </Button>
         <Button
