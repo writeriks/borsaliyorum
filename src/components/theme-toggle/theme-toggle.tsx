4b40ed2db34108cf53dcc1463887a35ba99d4213
sakirdemirhan <sakirdemirhan@outlook.com>
2024-06-30 20:57:13 +0200

    fix empty space

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -22,7 +22,7 @@ const ThemeModeToggle = () => {
           <Sun className="mr-2 h-4 w-4 dark:-rotate-90 dark:scale-100" />
         ) : (
           <Moon className="mr-2 h-4 w-4 dark:rotate-0 dark:scale-100" />
-        )}{" "}
+        )}
         {modeName}
       </Button>
     </div>
