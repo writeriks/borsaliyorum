c6c32c4b06015b04a84d226cb1f8db0d45e0a2bc
writeriks <emirhaktan@gmail.com>
2024-06-30 22:18:41 +0200

    fix styling for mid screen

--- src/components/ad-tags/inner-top-ad/inner-top-ad.tsx
+++ src/components/ad-tags/inner-top-ad/inner-top-ad.tsx
@@ -9,7 +9,7 @@ const InnerTopAd = () => {
   // make the call for ad if not mobile
 
   return (
-    <div className="flex h-32 lg:min-h-44 z-50 items-center bg-background sticky top-14 rounded-lg shadow-lg justify-center text-base">
+    <div className="flex h-32 lg:min-h-44 z-50 w-full items-center bg-background sticky top-14 rounded-lg shadow-lg justify-center text-base">
       Inner Top Ad
     </div>
   );
