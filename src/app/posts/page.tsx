ff17932b2de7fa943947e2b8e690787f18754cfd
writeriks <emirhaktan@gmail.com>
2024-07-01 10:58:38 +0200

    changes

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,10 +3,8 @@
 import React from "react";
 
 import NewPost from "@/components/new-post/new-post";
-import useUser from "@/hooks/useUser";
 
 const Home = () => {
-
   return (
     <div className="flex flex-col justify-center items-center">
       <NewPost />
--- src/components/new-post/new-post.tsx
+++ src/components/new-post/new-post.tsx
@@ -5,6 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 import useUser from "@/hooks/useUser";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Button } from "@/components/ui/button";
+import { ImagePlus } from "lucide-react";
 
 const NewPost = () => {
   const [idea, setIdea] = useState("");
@@ -13,7 +14,7 @@ const NewPost = () => {
   // TODO replace hook with redux selector. Get user from redux
   const user = useUser();
   const profileImage = user?.profilePhoto;
-  const proxyUrl = `/api/imageProxy?imageUrl=${encodeURIComponent(
+  const proxyUrl = `/api/imageProxy?imageUrlaS=${encodeURIComponent(
     profileImage as string
   )}`;
 
@@ -36,7 +37,9 @@ const NewPost = () => {
             src={proxyUrl}
             alt="profile picture"
           />
-          <AvatarFallback>{initials}</AvatarFallback>
+          <AvatarFallback className="relative top-2 p-2.5 bg-secondary rounded-full">
+            {initials}
+          </AvatarFallback>
         </Avatar>
       </div>
 
@@ -58,23 +61,12 @@ const NewPost = () => {
           </Button>
           <div className="flex space-x-2">
             <Button
-              title="upload image"
               className="flex items-center p-1 w-8 h-8 rounded-full"
               variant="default"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 0 24 24"
-                width="24px"
-                fill="#4F46E5"
-              >
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M21.02 5H19V2.98c0-.54-.44-.98-.98-.98h-.03c-.55 0-.99.44-.99.98V5h-2.01c-.54 0-.98.44-.99.98v.03c0 .55.44.99.99.99H17v2.01c0 .54.44.99.99.98h.03c.54 0 .98-.44.98-.98V7h2.02c.54 0 .98-.44.98-.98v-.04c0-.54-.44-.98-.98-.98zM16 9.01V8h-1.01c-.53 0-1.03-.21-1.41-.58-.37-.38-.58-.88-.58-1.44 0-.36.1-.69.27-.98H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8.28c-.3.17-.64.28-1.02.28-1.09-.01-1.98-.9-1.98-1.99zM15.96 19H6c-.41 0-.65-.47-.4-.8l1.98-2.63c.21-.28.62-.26.82.02L10 18l2.61-3.48c.2-.26.59-.27.79-.01l2.95 3.68c.26.33.03.81-.39.81z" />
-              </svg>
+              <ImagePlus />
             </Button>
             <Button
-              title="test"
               className="flex items-center p-2 w-8 h-8 rounded-full "
               variant="default"
             >
