830747d9ceb3ca7aa32f10045a46e5149a92b5c9
writeriks <emirhaktan@gmail.com>
2024-08-10 22:20:58 +0200

    Add check if user updates its own email

--- src/app/api/user/update-user-email/route.ts
+++ src/app/api/user/update-user-email/route.ts
@@ -1,11 +1,24 @@
 import { User } from '@/services/firebase-service/types/db-types/user';
+import { auth } from '@/services/firebase-service/firebase-admin';
 import prisma from '@/services/prisma-service/prisma-client';
+import { createResponse, ResponseStatus } from '@/app/api/api-utils/api-utils';
 
 // TODO: Consider renaming route
 export async function POST(request: Request): Promise<Response> {
   try {
+    const token = request.headers.get('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+      return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+    }
+
+    const idToken = await auth.verifyIdToken(token);
     const body = await request.json();
     const userData: User = body['user'];
+
+    if (idToken.uid !== userData.firebaseUserId) {
+      return createResponse(ResponseStatus.UNAUTHORIZED);
+    }
 
     await prisma.user.update({
       where: {
