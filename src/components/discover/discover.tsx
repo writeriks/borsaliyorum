ba5f78e260c4c4b3cea5c2607d50a63dc96018d1
writeriks <emirhaktan@gmail.com>
2025-01-06 20:56:44 +0100

    Ensure discover is fetched when user exist

--- src/components/discover/discover.tsx
+++ src/components/discover/discover.tsx
@@ -6,15 +6,21 @@ import { useQuery } from '@tanstack/react-query';
 import LoadingSkeleton from '@/components/loading-skeleton/loading-skeleton';
 import { LoadingSkeletons } from '@/app/constants';
 import TrendingTopics from '@/components/discover/trending-topics';
+import useUser from '@/hooks/useUser';
 
 const Discover: React.FC = () => {
+  const { fbAuthUser } = useUser();
+
   const { data: trends, isLoading } = useQuery({
     queryKey: ['get-trends'],
     queryFn: () => discoverApiService.getTrends(),
+    enabled: !!fbAuthUser,
   });
 
   const renderTrends = (): React.ReactNode => {
-    if (trends) {
+    if (!fbAuthUser) {
+      return null;
+    } else if (trends) {
       return <TrendingTopics trends={trends} />;
     } else if (isLoading) {
       return <LoadingSkeleton type={LoadingSkeletons.DISCOVER} />;
